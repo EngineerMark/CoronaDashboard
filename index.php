631256e080f07578114afb9a25447bd6999ea33f
Mark Meijer <engineermarkpro@gmail.com>
2020-10-12 18:41:11 +0200

    Preparations project

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="title" content="Netherlands COVID-19 Coronavirus Dashboard">
+        <meta name="description" content="Visualization of the COVID-19 pandemic in the Netherlands">
+        <meta name="keywords" content="corona, covid, coronavirus, netherlands, dutch, graphs, data, stats, statistics, graph">
+        <meta name="robots" content="index, follow">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <meta name="language" content="English">
+
+        <title>Coronavirus COVID-19 Dashboard</title>
+
+        <link rel="stylesheet" href="fontawesome/css/all.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/mdb.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <nav class="navbar navbar-light blue lighten-4">
+                <span class="navbar-brand"><i class="fas fa-virus"></i> COVID-19 Dashboard</span>
+            </nav>
+
+            <!-- Credits -->
+            <div class="card">
+
+            </div>
+        </div>
+    </body>
+</html>
