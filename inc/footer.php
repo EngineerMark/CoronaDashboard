f785d74eefc18ca8b6a6a3087daaf09e16fa2174
Mark Meijer <engineermarkpro@gmail.com>
2020-10-12 18:55:45 +0200

    Split content to include files

---
+++ inc/footer.php
@@ -0,0 +1,31 @@
+<div class="row">
+                <div class="col-lg-6">
+                    <!-- About -->
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">About</h4>
+                            <div class="card-text">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <!-- Credits -->
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title">Credits and Sources</h4>
+                            <div class="card-text">
+                                <ul>
+                                    <li>Coronavirus Datasets: <a target="_blank" href="https://rivm.nl/">RIVM <i class="fas fa-external-link-square-alt"></i></a></li>
+                                    <li><a target="_blank" href="https://mdbootstrap.com/">Material Design Bootstrap <i class="fas fa-external-link-square-alt"></i></a></li>
+                                    <li><a target="_blank" href="https://fontawesome.com/">Font Awesome <i class="fas fa-external-link-square-alt"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
